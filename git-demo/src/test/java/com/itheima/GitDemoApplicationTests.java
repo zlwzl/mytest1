6b20ed9cde2be7591f7dc8c1b291dceaab3dd670
zlwzl <1540851331@qq.com>
2019-01-03 19:06:14 +0800

    zlw

---
+++ git-demo/src/test/java/com/itheima/GitDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
